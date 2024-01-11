7f1e5a9053ffdffb04116c80e66f0299bf38ecd1
Habro <gkgud7@gmail.com>
2024-01-11 14:48:55 +0900

    Update body.jsx

--- src/pages/login/body.jsx
+++ src/pages/login/body.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import { StyledBody, Image, StyledLink } from './style'; // style.jsx 파일에서 가져오기
-
+//import { GoogleLogin } from 'react-google-login'; //다운받을지 말지...
+//npm install --save gapi-script//
+//import useScript from 'gapi-script'
 import boo_circle_1 from '../../assets/images/boo_circle_1.png';
 
 function Body() {
