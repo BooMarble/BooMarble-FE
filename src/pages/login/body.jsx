fb5f4c3bcae5739301244623c6fff18f84ffe6be
jimin1006 <129057172+jimin1006@users.noreply.github.com>
2024-01-14 22:13:42 +0900

    소셜로그인 구현 완료

--- src/pages/login/body.jsx
+++ src/pages/login/body.jsx
@@ -9,18 +9,10 @@ import axios from "axios";
 import { useLocation } from 'react-router-dom';
 
 function Body() {
-
-    // // 현재 페이지의 url을 추출
-    // const location = useLocation();
-    // console.log(location)
-    // axios.get(`https://boomarble.com/login/oauth2/code/google`).then((response) => {
-    //   console.log(response.data)
-    // })
-
   return (
     <StyledBody>
       <Image src={boo_circle_1} alt="boo_circle" />
-      <StyledLink as={Link} to="https://accounts.google.com/o/oauth2/auth?client_id=792908248831-o01redqdqoehht1v5ht5151ik4kog7a4.apps.googleusercontent.com&redirect_uri=https://boomarble.com/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile">google로 시작하기</StyledLink>
+      <StyledLink as={Link} to="https://accounts.google.com/o/oauth2/auth?client_id=792908248831-o01redqdqoehht1v5ht5151ik4kog7a4.apps.googleusercontent.com&redirect_uri=http://localhost:3000/login/oauth2/code/google&response_type=code&scope=https://www.googleapis.com/auth/userinfo.email%20https://www.googleapis.com/auth/userinfo.profile">google로 시작하기</StyledLink>
     </StyledBody>
   );
 }
